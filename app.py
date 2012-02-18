49a24fbb8a77fb03710204f648ad7fb5445b380f
Francesco Gigli <jaramir@gmail.com>
2012-02-18 03:47:15 +0100

    a wait page

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
+#!/usr/bin/python
+# coding: utf-8
+
 import os
+import flask
 
-from flask import Flask
-app = Flask(__name__)
+app = flask.Flask( __name__ )
 
-@app.route('/')
+@app.route( '/' )
 def hello():
-    return 'Hello World!'
+    return flask.render_template( "index.html" )
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int( os.environ.get( "PORT", 5000 ) )
+    app.run( "0.0.0.0", port )
