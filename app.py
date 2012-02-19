88f1c127826a0e6e17c094ffb469f8f3ac50fa85
Francesco Gigli <jaramir@gmail.com>
2012-02-19 17:19:05 +0100

    print it

--- app.py
+++ app.py
@@ -7,16 +7,13 @@ import urlparse
 
 app = flask.Flask( __name__ )
 
-global db
-db=False
-
 @app.route( '/' )
 def hello():
-    return flask.render_template( "index.html", db=db )
+    return flask.render_template( "index.html" )
 
 if __name__ == '__main__':
-
-    db = urlparse.urlparse( os.environ["DATABASE_URL"] )
+    db = urlparse.urlparse( os.environ.get( "DATABASE_URL", "" ) )
+    print db
 
     port = int( os.environ.get( "PORT", 5000 ) )
 
