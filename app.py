14a18837db0d6afaf50c9972dcfd792047f26d19
Francesco Gigli <jaramir@gmail.com>
2012-02-19 16:59:48 +0100

    testing env vars

--- app.py
+++ app.py
@@ -8,8 +8,12 @@ app = flask.Flask( __name__ )
 
 @app.route( '/' )
 def hello():
-    return flask.render_template( "index.html" )
+    return flask.render_template( "index.html", debug=app.debug )
 
 if __name__ == '__main__':
     port = int( os.environ.get( "PORT", 5000 ) )
+
+    if "DEBUG" in os.environ:
+        app.debug = True
+
     app.run( "0.0.0.0", port )
