20fd9820edfa8c552021f631a977c9cb9c575483
Francesco Gigli <jaramir@gmail.com>
2012-02-19 17:15:58 +0100

    dump db url

--- app.py
+++ app.py
@@ -3,13 +3,24 @@
 
 import os
 import flask
+import urlparse
 
 app = flask.Flask( __name__ )
 
+global db
+db=False
+
 @app.route( '/' )
 def hello():
-    return flask.render_template( "index.html" )
+    return flask.render_template( "index.html", db=db )
 
 if __name__ == '__main__':
+
+    db = urlparse.urlparse( os.environ["DATABASE_URL"] )
+
     port = int( os.environ.get( "PORT", 5000 ) )
+
+    if os.environ.get( "DEBUG", "0" ) == "1":
+        app.debug = True
+
     app.run( "0.0.0.0", port )
